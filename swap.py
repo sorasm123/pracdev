93c9e75d18ca80a12a8c3f364535c6497c99da3c
sorasm123 <swarasm1ga22cs173@gmail.com>
2025-08-06 08:57:25 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,12 @@
+# Simple swap using a temporary variable
+a = 5
+b = 10
+
+print("Before swapping: a =", a, "b =", b)
+
+# Swapping
+temp = a
+a = b
+b = temp
+
+print("After swapping: a =", a, "b =", b)
